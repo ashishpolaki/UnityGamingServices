7c65290b18d3a4f98990b3115da61ae79c2f2151
ashishpolaki <61271327+ashishpolaki@users.noreply.github.com>
2024-07-26 17:35:09 +0530

    Indicator messages in Race Schedule Tab
    Indicator messages in Race Schedule Tab, cloud code yet to do.

--- Assets/UGSTest/Scripts/UI/Screen Tabs/RaceScheduleTab.cs
+++ Assets/UGSTest/Scripts/UI/Screen Tabs/RaceScheduleTab.cs
@@ -19,6 +19,13 @@ namespace UI.Screen.Tab
         [SerializeField] private TimeAdjustmentSettings endSchedule;
         #endregion
 
+        #region Private Variables
+        private string raceStartSchedule;
+        private string raceEndSchedule;
+        private int timeGap;
+        private int preRaceWaitTime;
+        #endregion
+
         #region Unity Methods
         private void OnEnable()
         {
@@ -57,45 +64,97 @@ namespace UI.Screen.Tab
 
         private async void OnSetScheduleBtnClick()
         {
-            string scheduleStartString = $"{startSchedule.ReturnTime()}";
-            string scheduleEndString = $"{endSchedule.ReturnTime()}";
-            //Convert to Utc
-            scheduleStartString = ConvertToUTC(scheduleStartString);
-            scheduleEndString = ConvertToUTC(scheduleEndString);
-
-            // Check if start and end schedule strings are equal
-            if (scheduleStartString.Equals(scheduleEndString, StringComparison.OrdinalIgnoreCase))
+            if (!IsRaceScheduleValid() || !IsTimeGapValid() || !IsPreRaceWaitTimeValid())
             {
-                // If they are equal, return from the method
-                errorMessageTxt.text = "Start and End time cannot be the same";
-                return;
-            }
-
-
-            //Check if time gap is empty
-            if (string.IsNullOrEmpty(timeGap_Input.text))
-            {
-                errorMessageTxt.text = "Please enter the time gap";
-                return;
-            }
-
-            //Check if prerace wait time is empty
-            if (string.IsNullOrEmpty(preRaceWaitTime_Input.text))
-            {
-                errorMessageTxt.text = "Please enter the Lobby wait Time";
                 return;
             }
 
             //Set the schedule
             Func<Task> method = () => GameManager.Instance.CloudCode.ScheduleRaceTime(new UGS.CloudCode.HostScheduleRace
             {
-                ScheduleStart = scheduleStartString,
-                ScheduleEnd = scheduleEndString,
-                TimeGap = int.Parse(timeGap_Input.text),
-                PreRaceWaitTime = int.Parse(preRaceWaitTime_Input.text)
+                ScheduleStart = raceStartSchedule,
+                ScheduleEnd = raceEndSchedule,
+                TimeGap = timeGap,
+                PreRaceWaitTime = preRaceWaitTime
             });
             await LoadingScreen.Instance.PerformAsyncWithLoading(method);
             Close();
+        }
+
+        private bool IsRaceScheduleValid()
+        {
+            raceStartSchedule = ConvertToUTC(startSchedule.ReturnTime()); 
+            raceEndSchedule = ConvertToUTC(endSchedule.ReturnTime());
+
+            // Check if start and end schedule strings are equal
+            if (raceStartSchedule.Equals(raceEndSchedule, StringComparison.OrdinalIgnoreCase))
+            {
+                // If they are equal, return from the method
+                errorMessageTxt.text = "Start and End time cannot be the same";
+                return false;
+            }
+
+            return true;
+        }
+        private bool IsTimeGapValid()
+        {
+            //Check if time gap is empty
+            if (string.IsNullOrEmpty(timeGap_Input.text))
+            {
+                errorMessageTxt.text = "Please enter the time gap";
+                return false;
+            }
+            timeGap = int.Parse(timeGap_Input.text);
+
+            //Check if time gap is less than 0
+            if (timeGap <= 0)
+            {
+                errorMessageTxt.text = "The duration between races should be greater than zero";
+                return false;
+            }
+
+            //Check if duration between races is less than schedule time.
+            DateTime startTime = DateTime.ParseExact(raceStartSchedule, "HH:mm", CultureInfo.InvariantCulture);
+            DateTime endTime = DateTime.ParseExact(raceEndSchedule, "HH:mm", CultureInfo.InvariantCulture);
+            //If end time is less than start time, add a day to end time
+            if (endTime < startTime)
+            {
+                endTime = endTime.AddDays(1);
+            }
+            TimeSpan raceTimeSpan = endTime - startTime;
+            TimeSpan timeGapSpan = TimeSpan.FromMinutes(timeGap);
+            if (raceTimeSpan < timeGapSpan)
+            {
+                errorMessageTxt.text = "The duration between races should be less than the race schedule.";
+                return false;
+            }
+
+            return true;
+        }
+        private bool IsPreRaceWaitTimeValid()
+        {
+            //Check if prerace wait time is empty
+            if (string.IsNullOrEmpty(preRaceWaitTime_Input.text))
+            {
+                errorMessageTxt.text = "Please enter the Lobby wait Time";
+                return false;
+            }
+            preRaceWaitTime = int.Parse(preRaceWaitTime_Input.text);
+
+            //Check if prerace wait time is less than 0
+            if (preRaceWaitTime <= 0)
+            {
+                errorMessageTxt.text = "Lobby wait time should be greater than zero";
+                return false;
+            }
+
+            //Check if the prerace wait time is greater than the time gap
+            if (preRaceWaitTime >= timeGap)
+            {
+                errorMessageTxt.text = "Lobby wait time should be less than the time gap";
+                return false;
+            }
+            return true;
         }
         #endregion
     }
